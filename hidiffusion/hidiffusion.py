7531fbad005412933a71288058902962b703b28c
artmamedov <artmame@gmail.com>
2024-05-20 13:54:03 -0400

    Move import to be from utils

--- hidiffusion/hidiffusion.py
+++ hidiffusion/hidiffusion.py
@@ -6,7 +6,12 @@ import torch.nn.functional as F
 from .utils import isinstance_str
 from dataclasses import dataclass
 import diffusers
-from diffusers.utils import USE_PEFT_BACKEND, replace_example_docstring
+from diffusers.utils import (
+    USE_PEFT_BACKEND,
+    replace_example_docstring,
+    scale_lora_layers,
+    unscale_lora_layers,
+)
 from diffusers.pipelines.stable_diffusion_xl.pipeline_output import (
     StableDiffusionXLPipelineOutput,
 )
@@ -16,8 +21,6 @@ from diffusers.utils.torch_utils import (
     is_torch_version,
     randn_tensor,
     apply_freeu,
-    scale_lora_layers,
-    unscale_lora_layers,
 )
 from diffusers.pipelines.controlnet.multicontrolnet import MultiControlNetModel
 from diffusers.models import ControlNetModel
